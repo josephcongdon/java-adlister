827e9e79f92c02a83e79e88d79f3e598b07334dc
Joseph Congdon <jocongdon@yahoo.com>
2021-10-19 11:17:09 -0500

    clean up again

--- src/main/java/BeanTest.java
+++ src/main/java/BeanTest.java
@@ -3,7 +3,10 @@ public class BeanTest {
 
     public static void main(String[] args) {
 
+        Author a = new Author();
+        a.setFirstName("Greg");
 
+        System.out.println(a.getFirstName());
 
     }
 
